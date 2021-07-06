5f9bb467e9914a3581f5fcb030b5779a75cb2096
Austen Constable <awconstable@gmail.com>
2021-07-06 17:37:29 +0200

    Added git test code

--- src/main/java/team/releaseutil/ReleaseutilApplication.java
+++ src/main/java/team/releaseutil/ReleaseutilApplication.java
@@ -1,13 +1,29 @@
 package team.releaseutil;
 
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.revwalk.RevCommit;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.File;
+import java.io.IOException;
+
 @SpringBootApplication
-public class ReleaseutilApplication {
+public class ReleaseutilApplication implements CommandLineRunner
+	{
 
 	public static void main(String[] args) {
 		SpringApplication.run(ReleaseutilApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws IOException, GitAPIException
+		{
+		Git git = Git.open(new File("./"));
+		for(RevCommit commit: git.log().all().call()){
+		  System.out.println(commit.getName() + " : " + commit.getShortMessage());
+		} 
+		}
 }
