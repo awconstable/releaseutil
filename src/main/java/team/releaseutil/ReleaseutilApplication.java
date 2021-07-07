2ffe8e0a49a760deb9ea1ca5bce39f7983e6e4b0
Austen Constable <awconstable@gmail.com>
2021-07-07 15:18:57 +0200

    Calculate lead time

--- src/main/java/team/releaseutil/ReleaseutilApplication.java
+++ src/main/java/team/releaseutil/ReleaseutilApplication.java
@@ -3,34 +3,62 @@ package team.releaseutil;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.errors.GitAPIException;
 import org.eclipse.jgit.errors.RepositoryNotFoundException;
+import org.eclipse.jgit.lib.Ref;
 import org.eclipse.jgit.revwalk.RevCommit;
+import org.eclipse.jgit.revwalk.filter.RevFilter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.io.File;
 import java.io.IOException;
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.TemporalUnit;
+import java.util.List;
 
 @SpringBootApplication
 public class ReleaseutilApplication implements CommandLineRunner
 	{
-	private static final String GIT_REPO = "/tmp/git-repo";
+	@Value("${release.git.repo.location:/tmp/git-repo}")
+	private String gitRepo;
 	public static void main(String[] args) {
 		SpringApplication.run(ReleaseutilApplication.class, args);
 	}
 
 	@Override
-	public void run(String... args) throws IOException, GitAPIException
+	public void run(String... args) throws GitAPIException, IOException
+		{
+		outputReleaseNotes("", "", ZonedDateTime.now(ZoneOffset.UTC), "");
+		}
+
+	public void outputReleaseNotes(String prevReleaseTag, String version, ZonedDateTime deployTime, String rfcRef) throws GitAPIException, IOException
 		{
 		Git git;
 		try {
-			git = Git.open(new File(GIT_REPO));
+		System.out.println("Opening git repo " + gitRepo);
+		git = Git.open(new File(gitRepo));
 		} catch (RepositoryNotFoundException re){
-			System.out.println("Unable to find git repo " + GIT_REPO);
-			return;
+		System.out.println("Unable to find git repo " + gitRepo);
+		return;
 		}
-		for(RevCommit commit: git.log().all().call()){
-		  System.out.println(commit.getName() + " : " + commit.getShortMessage());
-		} 
-		}
+
+		List<Ref> tags = git.tagList().call();
+		System.out.println("List all tags. Count:  " + tags.size());
+		tags.forEach(t -> System.out.println(t.getName()));
+
+		Iterable<RevCommit> logs = git.log().setRevFilter(RevFilter.NO_MERGES).all().call();
+		System.out.println("List all logs without merges");
+		logs.forEach(r -> {
+			ZonedDateTime commitTime = ZonedDateTime.ofInstant(Instant.ofEpochSecond(r.getCommitTime()), ZoneOffset.UTC);
+			Duration leadTime = Duration.between(commitTime, deployTime);
+			System.out.println(
+					"id: " + r.getName() +
+					", message: " + r.getShortMessage() +
+					", time: " + DateTimeFormatter.ISO_LOCAL_DATE_TIME.format(commitTime) +
+					", lead time (secs): " + leadTime.getSeconds());
+		});
+
+	}
 }
